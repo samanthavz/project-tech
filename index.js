3979f0839bf84372f3e3574d63a3dde2254ce198
samantha van zandwijk <samanthavanzandwijk@gmail.com>
2021-04-20 16:48:27 +0200

    added templating engine

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
 const camelcase = require('camelcase');
-const express = require('express')
+const express = require('express');
+const pug = require('pug');
 const app = express()
-const port = 8080
+const port = 3000
 
-app.use(express.static('./static/public'))
+//pug
 
-app.get('/', (req, res) => {
-  res.send('Hello World and Bob!')
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Doggo app', message: 'This is my matching app' })
+})
+
+
+//expresssssssssssssssssssss
+
+// app.use(express.static('./static/public'))
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World and Bob!')
+// });
+
+//routes
+
+app.get('/home', (req, res) => {
+  res.render('home', { title: 'Doggo app', message: 'This is my home page' })
 });
 
-app.get('/test', (req, res) => {
-  res.send('This is a test')
+app.get('/list', (req, res) => {
+  res.render('index', { title: 'Doggo app', message: 'This is a list page for like doggos' })
 });
+
 
 //adding an id
 app.get('/test/:testId/:slug', (req, res) => {
   res.send(`<h1> this is page for ${req.params.slug}</h1>`)
 });
 
+
+//error handling
 app.use(function (req, res, next) {
   res.status(404).send("sorry can't find that!")
 });
 
-
+//connect
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
+
+
