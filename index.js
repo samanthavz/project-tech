bf330ded9768eda5e5080caf679f91102be82e2d
samantha van zandwijk <samanthavanzandwijk@gmail.com>
2021-05-08 14:37:29 +0200

    form working

--- index.js
+++ index.js
@@ -1,44 +1,40 @@
-const camelcase = require("camelcase");
+// const camelcase = require("camelcase");
 const express = require("express");
-const pug = require("pug");
+const bodyParser = require("body-parser");
+// const pug = require("pug");
 const app = express();
 const port = 3000;
 
+// TODO: this is data from database
 const liked = [
   { name: "Ben", imgSrc: "", content: "Woof", price: "$300" },
   { name: "Patricia", imgSrc: "", content: "Woof", price: "$300" },
   { name: "Bob", imgSrc: "", content: "Woof", price: "$300" },
 ];
 
-const doggoList = [
-  {
-    name: "Karel",
-    imgSrc: "",
-    breed: "insert breed",
-    price: "$300",
-    age: "3yr",
-    gender: "./images/male.png",
-  },
-  {
-    name: "Wim",
-    imgSrc: "",
-    breed: "insert breed",
-    price: "$300",
-    age: "3yr",
-    gender: "./images/male.png",
-  },
-  {
-    name: "Frans",
-    imgSrc: "",
-    breed: "insert breed",
-    price: "$300",
-    age: "3yr",
-    gender: "./images/male.png",
-  },
-];
+// TODO: this is also data from database
+var doggo = {
+  name: "Karel",
+  imgSrc: "",
+  content: "Woof",
+  breed: "insert breed",
+  price: "$300",
+  age: "3yr",
+  gender: "./images/male.png",
+  userId: 42,
+};
 
 //pug
 app.use(express.static(__dirname + "/static/public/"));
+
+// For parsing nested JSON objects
+// see https://stackoverflow.com/questions/24330014/bodyparser-is-deprecated-express-4 and http://expressjs.com/en/resources/middleware/body-parser.html
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 app.set("view engine", "pug");
 
@@ -60,11 +56,30 @@ app.get("/", function (req, res) {
 //routes
 
 app.get("/home", (req, res) => {
-  res.render("home", { title: "DoggoSwipe", doggoList });
+  // FIXME: get doggo from database for user on request
+
+  res.render("home", { title: "DoggoSwipe", doggo });
 });
 
-app.post("/home", (req, res) => {
-  res.render("home", { title: "DoggoSwipe", doggoList });
+app.post("/matches/liked", (req, res) => {
+  console.log(req.body);
+
+  // TODO: get new doggo from database, put in 'liked' list for user
+  liked.push(doggo);
+
+  // TODO: remove when getting doggo from database works
+  doggo = null;
+
+  res.redirect("/home");
+});
+
+app.post("/matches/disliked", (req, res) => {
+  console.log(req.body);
+
+  // TODO: put in disliked list in database
+  doggo = null;
+
+  res.redirect("/home");
 });
 
 app.get("/matches", (req, res) => {
